7a23172630576ba9bf491141c87c9ef08a289330
Oleg.Galaykovskiy <helgi.arney@gmail.com>
2018-03-05 22:16:23 +0200

    some tricks in design

--- w3Quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ w3Quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,18 +1,21 @@
 package quickcheck
 
-import common._
-
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen._
+import org.scalacheck.Prop._
 import org.scalacheck._
-import Arbitrary._
-import Gen._
-import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   lazy val genHeap: Gen[H] = for {
     arb <- arbitrary[Int]
     oof <- oneOf[H](const[H](empty), genHeap)
-  } yield insert(arb,oof)
+  } yield insert(arb, oof)
+
+  lazy val genNonEmptyHeap: Gen[H] = for {
+    h <- arbitrary[H]
+    x <- arbitrary[Int]
+  } yield if (isEmpty(h)) insert(x, h) else h
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
@@ -21,4 +24,17 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("Bogus5") = forAll(genNonEmptyHeap, genNonEmptyHeap) { (h1: H, h2: H) =>
+    val h1Min = findMin(h1)
+    val h2Min = findMin(h2)
+    val min = if (h1Min < h2Min) h1Min else h2Min
+
+    val h3 = meld(h1, h2)
+    findMin(h3) == min
+  }
 }
---
+++ w3Quickcheck/src/main/scala/ws/scalaCheck.sc
@@ -0,0 +1,12 @@
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
+import org.scalacheck._
+val propConcatList = forAll { (l1: List[Int], l2: List[Int]) => l1.size + l2.size == (l1 ::: l2).size }
+
+propConcatList.check
+
+val f: ((Int) => Boolean) = (n) => scala.math.sqrt(n * n) == n
+
+val propSqrt = forAll(f)
+
+propSqrt.check
